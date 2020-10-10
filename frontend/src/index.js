46722aec5ee3cbeea3d8cf84f0090f1eddb2e00d
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2020-10-10 16:27:31 +0530

    formated index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+
+import './index.css';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root')
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
+	document.getElementById('root')
 );
-
