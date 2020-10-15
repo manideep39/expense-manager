2ebb75bbeb9e83ba235e5239db687f081ab45acf
Manideep Pullalachervu <pmanideepnaidu@gmail.com>
2020-10-15 10:45:10 +0530

    added last5tra, pagina, filterTransById API calls

--- backend/transactions/models/transaction.js
+++ backend/transactions/models/transaction.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const moment = require('moment'); 
+const moment = require('moment');
 
 const { Schema } = mongoose;
 
@@ -10,17 +10,17 @@ const transactionSchema = new Schema({
 	},
 	title: {
 		type: String,
-		required: true,
+		required: true
 	},
 	type: {
 		type: String,
-		required: true,
+		required: true
 	},
 	amount: {
 		type: Number,
-		required: true,
+		required: true
 	},
-  timestamp: {
+	timestamp: {
 		type: Date,
 		default: moment().format()
 	}
--- backend/transactions/routes/transactions.js
+++ backend/transactions/routes/transactions.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { getTransactions, addTransaction, deleteTransaction, updateTransaction } = require('../controllers/transaction-controller');
+const { getTransactions, getLastFiveTransactions, addTransaction, deleteTransaction, updateTransaction } = require('../controllers/transaction-controller');
+const paginationResults = require('../middleware/pagination');
 
-router.get('/transactions', getTransactions);
+const Transaction = require('../models/transaction');
+
+router.get('/transactions/:id', paginationResults(Transaction), getTransactions);
+router.get('/transactions/last-five/:id', getLastFiveTransactions)
 router.post('/add-transaction', addTransaction);
 router.delete('/delete-transaction/:id', deleteTransaction);
 router.put('/update-transaction/:id', updateTransaction);
